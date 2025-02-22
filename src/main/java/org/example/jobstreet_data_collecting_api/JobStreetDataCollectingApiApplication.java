9180b1e454dfaeb1d594b5bad599dcdc800de4e1
wahwahmayphu <wahwahmayphu1997@gmail.com>
2025-02-22 22:04:52 +0630

    First Project Implementation

---
+++ src/main/java/org/example/jobstreet_data_collecting_api/JobStreetDataCollectingApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobstreet_data_collecting_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobStreetDataCollectingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobStreetDataCollectingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jobstreet_data_collecting_api/JobStreetDataCollectingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobstreet_data_collecting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobStreetDataCollectingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
